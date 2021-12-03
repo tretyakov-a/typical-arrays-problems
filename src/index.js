af102774a5f0967f2162c9a37250f311924100aa
Alexander Tretyakov <tpetyakov@gmail.com>
2021-12-03 13:32:18 +0300

    feat: implement required functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+const isArgumentValid = arg => arg !== undefined && arg.length > 0;
 
 exports.min = function min (array) {
-  return 0;
+  if (!isArgumentValid(array))
+    return 0;
+  return array.reduce((minValue, el) => (
+    minValue > el ? el : minValue
+  ), array[0]);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!isArgumentValid(array))
+    return 0;
+  return array.reduce((maxValue, el) => (
+    maxValue < el ? el : maxValue
+  ), array[0]);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!isArgumentValid(array))
+    return 0;
+  const sum = array.reduce((sum, el) => sum + el, 0);
+  return sum / array.length;
 }
